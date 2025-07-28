b80d81395b17e33ff9ed55748c89e324dcf840fb
RachidJedata <jedatarachid@gmail.com>
2025-07-28 19:34:05 +0100

    Include a ContextAPI for global state management

--- alx-project-0x05/components/layouts/Header.tsx
+++ alx-project-0x05/components/layouts/Header.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
 import Button from "../common/Button";
+import { usePathname } from "next/navigation";
+import { useCount } from "@/context/CountContext";
 
 const Header: React.FC = () => {
+
+  const pathname = usePathname()
+  const { count } = useCount()
+
+  // console.log(pathname);
   return (
     <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
@@ -11,14 +18,22 @@ const Header: React.FC = () => {
 
         {/* Button Group */}
         <div className="flex gap-4">
-          <Button
+          {
+            !["/counter-app"].includes(pathname) ? (
+              <>
+              <Button
             buttonLabel="Sign In"
             buttonBackgroundColor="red"
           />
           <Button
             buttonLabel="Sign Up"
             buttonBackgroundColor="blue"
-          />
+          /></>
+            ) : (
+              <p className=" font-semibold text-lg">Current count : {count}</p>
+            )
+          }
+
         </div>
       </div>
     </header>
