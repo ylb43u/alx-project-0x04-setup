ac4c35f16968f48119caba391985a3c9b30a41f0
RachidJedata <jedatarachid@gmail.com>
2025-07-28 19:48:39 +0100

    Include a Redux for global state management

---
+++ alx-project-0x06/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
